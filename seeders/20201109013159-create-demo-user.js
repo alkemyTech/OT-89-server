d15f9450098634d17e6dc03d2beab8eb7b62ffcc
Martin Cappi <mcappi91@gmail.com>
2021-10-27 19:50:05 -0300

    set User seed

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -1,26 +1,40 @@
-'use strict';
-
+"use strict";
+const bcrypt = require("bcrypt");
+const { Op } = require("sequelize");
+const { adminSeed, regularSeed } = require("./UserDataSeed");
+//FIXME: Make sure that hashing matches with the one at register/login routes
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
-      firstName: 'Usuario',
-      lastName: 'Demo',
-      email: 'test@test.com',
-      // Important: Password not encrypted yet! 
-      password: '1234',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    }], {});
-  },
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert(
+            "Users",
+            [...adminSeed, ...regularSeed],
+            {
+                hooks: {
+                    beforeCreate: async (user) => {
+                        if (user.password) {
+                            const salt = await bcrypt.genSaltSync(10, "a");
+                            user.password = bcrypt.hashSync(
+                                user.password,
+                                salt
+                            ); //replaces the incoming password for its hashed state before registering it into the database
+                        }
+                    },
+                },
+            }
+        );
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         */
+        await queryInterface.bulkDelete("Users", {
+            email: {
+                [Op.or]: {
+                    [Op.like]: "%.user@mail.com",
+                    [Op.like]: "%.admin@mail.com",
+                },
+            },
+        });
+    },
 };
