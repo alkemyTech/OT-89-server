b74fd321ca051103581774646ab73e78c3808fdb
Martin Cappi <mcappi91@gmail.com>
2021-10-27 19:49:36 -0300

    set (user) Role seed

--- seeders/20201109013158-create-roles.js
+++ seeders/20201109013158-create-roles.js
@@ -1,28 +1,29 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Roles', [
-      {
-        name: 'Admin',
-        description: 'Usuario administrador',
-        createdAt: new Date,
-        updatedAt: new Date
-      },
-      {
-        name: 'Standard',
-        description: 'Usuario regular',
-        createdAt: new Date,
-        updatedAt: new Date
-      }], {});
-  },
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert(
+            "Roles",
+            [
+                {
+                    roleId: 1,
+                    name: "Admin",
+                    description: "Usuario administrador",
+                },
+                {
+                    roleId: 2,
+                    name: "Standard",
+                    description: "Usuario regular",
+                },
+            ],
+            {}
+        );
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         */
+        await queryInterface.bulkDelete("Roles", { roleId: [1, 2] });
+    },
 };
