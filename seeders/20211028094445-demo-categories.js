814710f62b7f3c0d7b0c94c309e9697cb00e3fde
Chamarez <michel.chamarez@gmail.com>
2021-10-29 00:30:16 +0200

    Fixed model

--- seeders/20211028094445-demo-categories.js
+++ seeders/20211028094445-demo-categories.js
@@ -13,6 +13,6 @@ module.exports = {
     ]);
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("Users", null, {});
+    return queryInterface.bulkDelete("Categories", null, {});
   },
 };
