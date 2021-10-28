be2364445724beb2caea909bb9d041ad54fcd699
Martin Cappi <mcappi91@gmail.com>
2021-10-28 12:07:31 -0300

    fix user query for sequelize-cli seed:undo and more failed hooks

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -8,26 +8,31 @@ const User = db.sequelize.models.User;
 //FIXME: Make sure that hashing matches with the one at register/login routes
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.bulkInsert("Users", [...adminSeed, ...regularSeed], {
-        //await User.bulkCreate([...adminSeed, ...regularSeed], {
-            hooks: { //FIXME: hooks are not working
-                beforeBulkCreate: async (bulk) => {
-                    return await bulk.map(async (user) => {
-                        throw new Error(`entre a Hooks ${user}`)
-                        if (user.password) {
-                            const salt = await bcrypt.genSaltSync(10, "a");
-                            user.password = bcrypt.hashSync(
-                                user.password,
-                                salt
-                            ); //replaces the incoming password for its hashed state before registering it into the database
-                            return user;
-                        }
-                    });
-                },
-            },
-            individualHooks:{ //FIXME: hooks are not working
-                beforeCreate: async(user)=>{throw new Error(`entre a individualHooks ${user}`)}
-            }
+        //await queryInterface.bulkInsert("Users", [...adminSeed, ...regularSeed], {
+        await User.bulkCreate([...adminSeed, ...regularSeed], {
+            // hooks: {
+            //     //FIXME: hooks are not working
+            //     beforeBulkCreate: async (bulk) => {
+            //         return await bulk.map(async (user) => {
+            //             throw new Error(`entre a Hooks ${user}`);
+            //         });
+            //     },
+            // },
+            // individualHooks: {
+            //     //FIXME: hooks are not working
+            //     beforeCreate: async (user) => {
+            //         throw new Error(`entre a individualHooks ${user}`);
+            //     },
+            // },
+            // beforeBulkCreate: async (bulk) => {
+            //     return await bulk.map(async (user) => {
+            //         throw new Error(`entre a Hooks ${user}`);
+            //     });
+            // },
+            // //FIXME: hooks are not working
+            // beforeCreate: async (user) => {
+            //     throw new Error(`entre a individualHooks ${user}`);
+            // },
         });
     },
 
@@ -36,12 +41,10 @@ module.exports = {
          * Add commands to revert seed here.
          */
         await queryInterface.bulkDelete("Users", {
-            email: {
-                [Op.or]: {
-                    [Op.like]: "%.user@mail.com",
-                    [Op.like]: "%.admin@mail.com",
-                },
-            },
+            [Op.or]: [
+                { email: { [Op.like]: "%.user@mail.com" } },
+                { email: { [Op.like]: "%.admin@mail.com" } },
+            ],
         });
     },
 };
