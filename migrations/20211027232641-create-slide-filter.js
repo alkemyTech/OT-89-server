81dfeeb15cd3b1956edc26274dd8df34a3b52aa1
Martin Cappi <mcappi91@gmail.com>
2021-10-27 21:09:00 -0300

    set SlideFilter model migration

--- migrations/20211027232641-create-slide-filter.js
+++ migrations/20211027232641-create-slide-filter.js
@@ -1,27 +1,32 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('SlideFilters', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      slideId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('SlideFilters');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable("SlideFilters", {
+            filterId: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: DataTypes.STRING(25),
+                allowNull: false,
+            },
+            description: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable("SlideFilters");
+    },
 };
