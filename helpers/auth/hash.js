4795a6fecc8a0d2e8d3b2cd4ee453170c958d006
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2021-11-03 08:29:33 -0300

    Update hash.js

--- helpers/auth/hash.js
+++ helpers/auth/hash.js
@@ -3,6 +3,3 @@ const bcrypt = require("bcrypt");
 // compares incoming password with one from the database
 const CompareHash = async (password, hashedPassword) => await bcrypt.compare(password, hashedPassword);
 
-module.exports = {
-  CompareHash,
-}
