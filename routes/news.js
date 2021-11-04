6a50bcbfbb549b327d7aae8b8ce7909ff5e680b8
Martin Cappi <mcappi91@gmail.com>
2021-11-03 23:20:01 -0300

    list of news endpoint ready to test

--- routes/news.js
+++ routes/news.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const db = require("../models/index");
-//news controller imports
 //news/:id controller imports
 
 const router = express.Router();
 const Entry = db.sequelize.models.Entry;
 
-//@DESC array of news
+//@DESC News List
 //@ROUTE /news
 //@METHOD GET
-router("/").get(middleware, controller);
+router("/").get(async (req, res, next) => {
+    try {
+        const news = await Entry.findAll({
+            attributes: ["name", "image", "createdAt"],
+            where: { type: "news" },
+            order: [["createdAt", DESC]],
+        });
+
+        if (!news) {
+            res.status(404);
+            throw new Error("There's no data to be found");
+        }
+        res.status(200).json(news);
+    } catch (err) {
+        next(err);
+    }
+});
 
 //@DESC single news by id
 //@ROUTE /news/:id
