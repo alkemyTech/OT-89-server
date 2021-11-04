6450d29f3337156e4cc66b790372136491dd6f19
Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-11-04 10:09:45 -0300

    Deleted textApi, and modificated dev and prod, because they are an object

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 //Será la api key cuando la tengamos en el entorno de producción
 const prodConfig={
-    SENDGRID_API_KEY=process.env.SENDGRID_API_KEY
+    SENDGRID_API_KEY:process.env.SENDGRID_API_KEY
 }
 module.exports=prodConfig;
