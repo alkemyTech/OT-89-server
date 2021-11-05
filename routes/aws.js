bcea205ea21ba1bb85526073e0555d51f43fcdab
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-05 16:28:04 -0300

    POST image Route added
    POST request has been handled with multer library, and the Key of the file uses uuid

--- routes/aws.js
+++ routes/aws.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer = require("multer");
+const uuid = require("uuid").v4;
 
 const AWS = require("aws-sdk");
 const awsConfig = require("../config/aws.config");
@@ -39,6 +41,37 @@ router.get("/:id", async (req, res) => {
   } catch (err) {
     console.log(err, err.message);
   }
+});
+
+// POST
+const storage = multer.memoryStorage({
+  destination: (req, file, cb) => {
+    cb(null, ".");
+  },
+});
+
+const upload = multer({ storage }).single("image");
+
+router.post("/upload", upload, (req, res) => {
+  const { originalname, buffer } = req.file;
+
+  const fileName = originalname.split(".");
+  const fileExt = fileName[fileName.length - 1];
+
+  const params = {
+    Key: `${uuid()}.${fileExt}`,
+    Body: buffer,
+    Bucket: awsConfig.bucketName,
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    }
+
+    console.log(data);
+    data.Key = res.status(200).json({ data });
+  });
 });
 
 module.exports = router;
