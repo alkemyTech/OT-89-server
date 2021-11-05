484d378a954f4b97ddbccdbb4aed620a1b5e6609
Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-11-05 11:43:24 -0300

    modifiqued api key, and html text from to sendMail

--- config/prod.js
+++
@@ -1,5 +0,0 @@
-//Será la api key cuando la tengamos en el entorno de producción
-const prodConfig={
-    SENDGRID_API_KEY:process.env.SENDGRID_API_KEY
-}
-module.exports=prodConfig;
--- services/sendMail.js
+++ services/sendMail.js
@@ -1,15 +1,14 @@
 const sgMail = require('@sendgrid/mail')
-const config = require('../config')
 
-sgMail.setApiKey('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') //ApiKey created in sendGrid
+sgMail.setApiKey('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') //ApiKey created in sendGrid
 function sendMail(to, subject, text) {
 
     const msg = {
         to: to,
-        from: 'somosfundacionmas@gmail.com', // Use the email address or domain you verified above
+        from: 'somosfundacionmas.argentina@gmail.com', // Use the email address or domain you verified above
         subject: subject,
         text: text,
-        html: `<strong>${text}}</strong>`,
+        html: `<p>${text}</p>`,
     }
     sgMail.send(msg).then(() => {}, error => {
         console.log(error);
@@ -18,5 +17,6 @@ function sendMail(to, subject, text) {
         }
     })
 };
+
 
 module.exports = sendMail;
