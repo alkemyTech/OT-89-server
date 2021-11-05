679795e7e592db6d722dd46e3429da202cc1c02a
Martin Cappi <mcappi91@gmail.com>
2021-11-04 21:40:05 -0300

    improve handling of bad requests
    switched query to findOne(), because findByPk() doesn't take where filters impeding filtering by news

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -14,18 +14,23 @@ const NewsById = async (req, res, next) => {
     try {
         const id = parseInt(req.params.id);
         if (isNaN(id)) {
-            res.status(400).json({ error: "Id must be a valid number" });
+            res.status(400).json({ error: "Id must be valid!" });
             return;
         }
 
-        const news = await Entry.findByPk(id);
+        const news = await Entry.findOne({ where: { type: "news", id: id } });
 
-        if (!news) {
-            res.status(400).json({ message: "Id not found" });
-        } else if (news.length == 0) {
-            res.status(204);
-        } else {
+        if (news) {
             res.status(200).json({ message: "Ok!", data: news });
+        } else {
+            const newsByPk = await Entry.findByPk(id);
+            if (!newsByPk) {
+                res.status(400).json({ message: "Id not found!" });
+            } else if (newsByPk) {
+                res.status(400).json({ message: "This is not a news item!" });
+            } else {
+                throw new Error("Unexpected.");
+            }
         }
     } catch (err) {
         next(err);
