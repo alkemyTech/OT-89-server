ecd0dc71468512d35f095f01c7821e0982df510d
Martin Cappi <mcappi91@gmail.com>
2021-11-11 15:37:40 -0300

    refine model

--- models/member.js
+++ models/member.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const { stringValidation } = require("../helpers/validation/modelValidation");
+
 module.exports = (sequelize, DataTypes) => {
   class Member extends Model {
     /**
@@ -12,13 +12,31 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Member.init({
-    name: DataTypes.STRING,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Member',
-  });
+  }
+  Member.init(
+    {
+      name: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+        validate: {
+          is: {
+            args: stringValidation(2, 45),
+            msg: "Invalid name",
+          },
+        },
+      },
+      imageUrl: {
+        type: DataTypes.STRING(500),
+        allowNull: true,
+        validate: { isUrl: true },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Member",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
   return Member;
 };
