5052b1d43c993e52209a68c2ca3aea0b4d730215
chamarez <michel.chamarez@gmail.com>
2021-11-11 02:24:57 +0100

    Add router in app

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const newsRouter = require('./routes/news')
 const authRouter = require('./routes/auth');
+const testimonialRouter = require('./routes/testimonial')
 
 const app = express();
 app.use(cors())
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter)
 app.use('/auth', authRouter);
+app.use('/testimonial', testimonialRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
