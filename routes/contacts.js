01c246ea4d5a9862aef53d3d4e4f8b8f520cc7e9
Martin Cappi <mcappi91@gmail.com>
2021-11-12 16:31:56 -0300

    set admin authentication

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { ContactsList } = require("../controllers/ContactsController");
+const IsAdmin = require("../helpers/auth/isAdmin");
 const IsAuthenticated = require("../helpers/auth/isAuthenticated");
 
 const router = express.Router();
 
 //@DESC Brings the whole list of contacts
 //@ROUTE /contacts
-router.route("/").get(IsAuthenticated, ContactsList);
+router.route("/").get(IsAuthenticated, IsAdmin, ContactsList);
 
 module.exports = router;
