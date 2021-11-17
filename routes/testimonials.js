af0835600631db266bd78a62e5ac34c792ab1ae4
Juan Bautista Abal <keroxx3@gmail.com>
2021-11-17 12:24:05 -0300

    Ahora verifica Admin y Auth en post testimonials

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,21 +1,23 @@
 //endpoint testimonials
 const express = require('express');
+const IsAdmin = require('../helpers/auth/isAdmin');
+const IsAuthenticated = require('../helpers/auth/isAuthenticated');
 const router = express.Router();
 const db = require('../models/index')
 const Testimonials = db.sequelize.models.testimonials;
 
-router.post('/', (req, res) => {
+router.post('/', IsAuthenticated, IsAdmin, (req, res) => {
     if (req.body.name && req.body.content) {
         Testimonials.create({
             name: req.body.name,
             content: req.body.content
         }).then(() => {
-            res.send({message: 'Testimonial created'})
+            res.send({ message: 'Testimonial created' })
         }).catch(err => {
-            res.send({message: err})
+            res.send({ message: err })
         })
     } else {
-        res.send({message: 'Missing data'})
+        res.send({ message: 'Missing data' })
     }
 });
 
