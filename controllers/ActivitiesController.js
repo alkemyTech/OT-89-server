426959687edb6e6738ac080fa3e871ccaff08d58
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-17 12:29:57 -0300

    PUT route to update activity added
    admin validator added as well

--- controllers/ActivitiesController.js
+++ controllers/ActivitiesController.js
@@ -1,4 +1,5 @@
 const db = require("../models/index");
+const { isEmpty } = require("lodash");
 
 const Activity = db.sequelize.models.Activity;
 
@@ -25,4 +26,46 @@ const postActivity = async (req, res, next) => {
   }
 };
 
-module.exports = { postActivity };
+const updateActivity = async (req, res, next) => {
+  try {
+    const { name, image, content } = req.body;
+    const { id: activityId } = req.params;
+
+    const isActivity = await Activity.findByPk(activityId);
+
+    if (!isActivity) {
+      res.status(404).json({ message: "Activity doesn't exist" });
+      return;
+    }
+
+    const payload = {};
+    if (name) payload.name = name;
+    if (image) payload.image = image;
+    if (content) payload.content = content;
+
+    if (isEmpty(payload)) {
+      res.status(400).json({
+        message:
+          "Either a name, image or content is required to perform an update!",
+      });
+    }
+
+    const updatedActivity = await Activity.update(payload, {
+      where: { id: activityId },
+      validate: true,
+    });
+
+    if (updatedActivity) {
+      res.status(200).json({
+        message: "Activity updated succesfully!",
+        data: payload,
+      });
+    } else {
+      throw new Error("Ups! Something went wrong");
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { postActivity, updateActivity };
--- routes/activities.js
+++ routes/activities.js
@@ -5,8 +5,10 @@ const isAdmin = require("../helpers/auth/IsAdmin");
 const IsAuthenticated = require("../helpers/auth/isAuthenticated");
 
 const { postActivity } = require("../controllers/ActivitiesController");
+const { updateActivity } = require("../controllers/ActivitiesController");
 
 // POST activities to the database validation information
 router.route("/").post(IsAuthenticated, isAdmin, postActivity);
+router.route("/:id").put(IsAuthenticated, isAdmin, updateActivity);
 
 module.exports = router;
