a96b3737f1d2446ab5e11b0b0a11c48f591f2cc3
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-11-17 15:01:11 -0300

    Modularized create category endpoint

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -1,34 +1,10 @@
 const express = require("express");
-const db = require("../models/index");
-const Categories = db.sequelize.models.Categories;
-const IsAuthenticated = require('../helpers/auth/isAuthenticated');
-const IsAdmin = require('../helpers/auth/isAdmin');
+// const IsAuthenticated = require('../helpers/auth/isAuthenticated');
+// const IsAdmin = require('../helpers/auth/isAdmin');
+const {createCategory} = require("../controllers/CategoriesController");
 
 const router = express.Router();
 
-///////////////////////////////////////////////////////POST CATEGORIE
-
-router.post("/", IsAuthenticated, IsAdmin, async (req, res) => {
-    try{
-        const name = req.body.name.trim();
-        const description = req.body.description.trim();
-
-        if(!name || !description){
-            res.status(400).json({ message: "All fields must be completed" });
-        }else{
-            const response = await Categories.create({
-                name: name,
-                description: description
-            },
-            {
-                fields: ["name", "description"]
-            });
-            res.status(201).json(response);
-        }
-    }
-    catch(e){
-        res.status(404).json({ message: e.message });
-    }
-});
+router.route("/").post( createCategory);
 
 module.exports = router;
