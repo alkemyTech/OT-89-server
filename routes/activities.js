5ee97e2cc3d1c75a92419ba44aeeb3abb118389e
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-17 11:24:43 -0300

    add token and admin validator

--- routes/activities.js
+++ routes/activities.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const isAdmin = require("../helpers/auth/IsAdmin");
+const IsAuthenticated = require("../helpers/auth/isAuthenticated");
+
 const { postActivity } = require("../controllers/ActivitiesController");
 
 // POST activities to the database validation information
-router.route("/").post(postActivity);
+router.route("/").post(IsAuthenticated, isAdmin, postActivity);
 
 module.exports = router;
