056b5da8249310893aa3c9a926e15729394ee1f6
chamarez <michel.chamarez@gmail.com>
2021-11-17 16:04:55 +0100

    First commit model created, endpoint modified

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Organizations = require('../models/Organizations')
+const Organizations = require('../models/organization')
 
 /* GET home page. */
 router.get('/:id/public', async (req, res, next) => {
-    const {name, image, phone, address, welcomeText} = await Organizations.findAll({
+    const {name, image, phone, address, welcomeText, instagram, facebook} = await Organizations.findAll({
         where: {
             organizationId: req.params.id
         }
     })
-    res.send({name, image, phone, address, welcomeText})
+    res.send({name, image, phone, address, welcomeText, instagram, facebook})
 });
 
 module.exports = router;
