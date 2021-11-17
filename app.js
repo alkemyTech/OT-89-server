b5785a4f722bf4a3bb5ef92d6ab6e20baaad02cc
Juan Bautista Abal <keroxx3@gmail.com>
2021-11-17 12:21:05 -0300

    Category to Categories

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const authRouter = require("./routes/auth");
 const contactsRouter = require("./routes/contacts")
 const awsRouter = require("./routes/aws");
 const memberRouter = require('./routes/member');
-const categoryRouter = require('./routes/category');
+const categoriesRouter = require('./routes/categories');
 const testimonialsRouter = require('./routes/testimonials');
 
 
@@ -38,7 +38,7 @@ app.use("/auth", authRouter);
 app.use("/contacts", contactsRouter);
 app.use("/aws", awsRouter);
 app.use('/members', memberRouter);
-app.use('/category', categoryRouter);
+app.use('/categories', categoriesRouter);
 app.use("/testimonials", testimonialsRouter);
 
 
