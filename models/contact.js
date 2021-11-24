de6180d5bfb9a6be13d3788b9dc172f405726712
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2021-11-24 18:46:06 -0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
-'use strict';
-const { stringValidation } = require('../helpers/validation/modelValidation')
-const { Model } = require('sequelize');
+"use strict";
+const { stringValidation } = require("../helpers/validation/modelValidation");
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Contact extends Model {
@@ -12,51 +12,51 @@ module.exports = (sequelize, DataTypes) => {
     // static associate(models) {
     //   // define association here
     // }
-  };
-  Contact.init({
+  }
+  Contact.init(
+    {
       name: {
         type: DataTypes.STRING(100),
         allowNull: false,
         validate: {
           is: {
-              args: stringValidation(2, 100),
-              msg: "Invalid name",
+            args: stringValidation(2, 100),
+            msg: "Invalid name",
           },
-        }
+        },
       },
       phone: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING(15),
         allowNull: false,
         validate: {
-          isInt: true,
-        }
-      }, 
+          args: stringValidation(10, 15),
+        },
+      },
       email: {
         type: DataTypes.STRING(100),
         allowNull: false,
         validate: {
           isEmail: true,
-        }
+        },
       },
       message: {
         type: DataTypes.STRING(2000),
         allowNull: false,
         validate: {
           is: {
-              args: stringValidation(2, 2000),
-              msg: "Invalid text",
+            args: stringValidation(2, 2000),
+            msg: "Invalid text",
           },
-        }
-      }, 
+        },
+      },
       deletedAt: {
         type: DataTypes.DATE,
-        allowNull: true
-      }
-
+        allowNull: true,
+      },
     },
     {
       sequelize,
-      modelName: 'Contact',
+      modelName: "Contact",
     }
   );
   return Contact;
