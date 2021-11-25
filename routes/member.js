e53d875977a0262754f8357d76e01dac241dadef
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-24 21:25:30 -0300

    auth added

--- routes/member.js
+++ routes/member.js
@@ -13,18 +13,18 @@ const router = express.Router();
 
 //@DESC create a new member for the organization
 //@ROUTE /members
-router.route("/").post(nameValidation, CreateMember);
+router.route("/").post(IsAdmin, nameValidation, CreateMember);
 
 //@DESC retrieve a list of all members of the organization
 //@ROUTE /members
-router.route("/").get(MembersList);
+router.route("/").get(IsAdmin, MembersList);
 
 //@DESC update a member's information
 //@ROUTE /members/:id
-router.route("/:id").put(UpdateMember);
+router.route("/:id").put(IsAdmin, UpdateMember);
 
 //@DESC delete a member from the organization
 //@ROUTE /members/:id
-router.route("/:id").delete(DeleteMember);
+router.route("/:id").delete(IsAdmin, DeleteMember);
 
 module.exports = router;
